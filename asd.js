9094120a9d8e3bee52fa40cb789973dc2355bf83
sebas felipe <sebasfer@outlook.com>
2022-06-10 15:22:09 -0500

    Cibax

---
+++ asd.js
@@ -0,0 +1,20 @@
+
+function toCamelCase(str){
+    x=str.split("-")
+    console.log(x[1])
+      //for(let i=0; i<x.length; i++){ 
+          
+    
+  }
+  
+  
+  toCamelCase('')
+  //toCamelCase("the_stealth_warrior")
+  toCamelCase("The-Stealth-Warrior")
+  toCamelCase("A-B-C")
+
+
+<input type="text" name="" id="">
+  
+  
+  
